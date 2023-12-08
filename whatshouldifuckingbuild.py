82c7d6b97b623b61c69d1836e53b7dfecd6980da
vivek-dodia <vivek.dodia@icloud.com>
2023-12-08 13:15:41 -0500

    adding rate limiting and geo restrictions

--- whatshouldifuckingbuild.py
+++ whatshouldifuckingbuild.py
@@ -1,14 +1,35 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, abort
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import requests
 import openai
 from dotenv import load_dotenv
 import os
+import geoip2.database
 
 load_dotenv()  # This loads the environment variables from .env
 
 app = Flask(__name__)
-
+limiter = Limiter(key_func=get_remote_address, default_limits=["200 per day", "50 per hour"])
+limiter.init_app(app)
 api_key = os.getenv('OPENAI_API_KEY')
+GEOIP_DB_PATH = 'path/to/GeoLite2-Country.mmdb'  # Update with the path to your GeoLite2 database
+
+def get_country(ip_address):
+    try:
+        with geoip2.database.Reader(GEOIP_DB_PATH) as reader:
+            response = reader.country(ip_address)
+            return response.country.iso_code
+    except Exception as e:
+        print(f"Error getting country from IP: {e}")
+        return None
+
+@app.before_request
+def block_country():
+    if not request.remote_addr.startswith(('127.', '192.', '10.')):  # Skip local IPs
+        country = get_country(request.remote_addr)
+        if country not in ['US', 'CA']:  # Allow only US and Canada
+            abort(403)  # Forbidden access
 
 def call_gpt_api(keywords):
     prompt = f"Generate three Python project ideas based on the following technologies: {keywords}. Categorize the ideas into beginner, advanced, and expert levels."
@@ -31,22 +52,17 @@ def call_gpt_api(keywords):
     else:
         return "Error: Unable to generate idea"
 
-
-    
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/generate_idea', methods=['POST'])
+@limiter.limit("5 per hour")  # Apply rate limiting to this endpoint
 def generate_idea():
-    print("generate_idea route called")  # Add this line
+    print("generate_idea route called")
     user_input = request.json['keywords']
     generated_idea = call_gpt_api(user_input)
     return jsonify({'idea': generated_idea})
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-    
-
